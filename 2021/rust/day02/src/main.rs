dd513a86e68e990ee3b99c842cf7cea9b5c7b773
Jeff Wang <jeffawang@gmail.com>
2021-12-10 00:24:46 -0700

    rust: day02 refactor

--- 2021/rust/day02/src/main.rs
+++ 2021/rust/day02/src/main.rs
@@ -1,24 +1,23 @@
-use std::{collections::HashMap, fs, hash::Hash};
+use std::fs;
 
-type position = (usize, usize);
-
-fn charMap(current: position, direction: &str, amount: usize) -> position {
-    match direction {
-        "forward" => (current.0 + amount, current.1),
-        "down" => (current.0, current.1 + amount),
-        "up" => (current.0, current.1 - amount),
-        _ => current,
+fn part1(input: &str) -> usize {
+    let mut x = 0;
+    let mut z = 0;
+    for line in input.lines() {
+        let tokens: Vec<&str> = line.split(" ").collect();
+        let i = tokens[1].parse::<usize>().unwrap();
+        match tokens[0] {
+            "forward" => x += i,
+            "down" => z += i,
+            "up" => z -= i,
+            _ => {}
+        }
     }
+    x * z
 }
 
 fn main() {
     let f = fs::read_to_string("../../inputs/day02").expect("oops");
-    let mut p: position = (0, 0);
-    for line in f.lines() {
-        let mut x = line.split_whitespace();
-        let direction = x.next().unwrap();
-        let amount: usize = x.next().unwrap().parse().unwrap();
-        p = charMap(p, direction, amount);
-    }
-    println!("{}", p.0 * p.1)
+    let p1 = part1(f.as_str());
+    println!("Part 1: {}", p1)
 }
