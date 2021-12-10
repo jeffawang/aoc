36571c51f5a9c8ed63177d7e2664de276de7668b
Jeff Wang <jeffawang@gmail.com>
2021-12-10 00:30:19 -0700

    rust: day02 part 2

--- 2021/rust/day02/src/main.rs
+++ 2021/rust/day02/src/main.rs
@@ -16,8 +16,28 @@ fn part1(input: &str) -> usize {
     x * z
 }
 
+fn part2(input: &str) -> usize {
+    let mut x = 0;
+    let mut z = 0;
+    let mut aim = 0;
+    for line in input.lines() {
+        let tokens: Vec<&str> = line.split(" ").collect();
+        let i: usize = tokens[1].parse::<usize>().unwrap();
+        match tokens[0] {
+            "forward" => {
+                x += i;
+                z += aim * i;
+            }
+            "down" => aim += i,
+            "up" => aim -= i,
+            _ => {}
+        }
+    }
+    x * z
+}
+
 fn main() {
     let f = fs::read_to_string("../../inputs/day02").expect("oops");
-    let p1 = part1(f.as_str());
-    println!("Part 1: {}", p1)
+    println!("Part 1: {}", part1(f.as_str()));
+    println!("Part 2: {}", part2(f.as_str()));
 }
